76d5f361729a8da6732f98ba9d9c8d18ec705c25
Alfredo <fredy.cracker@gmail.com>
2018-04-05 14:16:14 -0500

    primera animacion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+    var alto =$(window).height();
+    var ancho =$(window).width();
+    if(alto > ancho){
+       $("html").addClass("vr");
+
+    }
+    var controller = new ScrollMagic.Controller ({});
+
+    /* Escena Robot */
+    var sceneRobot = new ScrollMagic.Scene({triggerElement: "#escena_1", offset:alto/4, duration:alto})
+    .setPin("#robot")
+    .addIndicators()
+    .addTo(controller);
+    
 
 });
