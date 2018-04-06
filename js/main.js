4228b716215fd2ed366deeb6311d22f897f049b0
Alfredo <fredy.cracker@gmail.com>
2018-04-06 13:26:56 -0500

    animacion 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    setDebug();
     var alto =$(window).height();
     var ancho =$(window).width();
     if(alto > ancho){
@@ -14,20 +15,38 @@ $(document).ready(function(){
     .addTo(controller);
     
 
-    /* Escena Robot */
+    /* Escena Layout */
     var tweenLayout = new TimelineMax ()
     .add([
         TweenMax.fromTo("#layout", 1, {x:-ancho/4}, {x:ancho/5}),
         TweenMax.fromTo("#layout", 0.3, {opacity:0}, {opacity:1}),
     ]);
-    var sceneTweenLayout = new ScrollMagic.Scene({triggerElement: "#escena_2", offset:0, duration:alto})
+    var sceneTweenLayout = new ScrollMagic.Scene({triggerElement: "#escena_2", offset:0, duration:alto/2})
     .setTween(tweenLayout)
     .addIndicators()
     .addTo(controller);
-    var scenePinLayout = new ScrollMagic.Scene({triggerElement: "#escena_2", offset:alto/3, duration:alto*2.3})
+    var scenePinLayout = new ScrollMagic.Scene({triggerElement: "#escena_2", offset:alto/3, duration:alto*2.6})
     .setPin("#layout")
     .addIndicators()
     .addTo(controller);
-    
+
+    /* Escena BD */
+    var tweenBD = new TimelineMax ()
+    .add([
+        TweenMax.fromTo("#BD", 0.5, {x:-ancho/4}, {x:ancho/3}),
+    ]);
+    var sceneTweenBD = new ScrollMagic.Scene({triggerElement: "#escena_3", offset:0, duration:alto/2})
+    .setTween(tweenBD)
+    .addIndicators()
+    .addTo(controller);
+    var scenePinLayout = new ScrollMagic.Scene({triggerElement: "#escena_3", offset:alto/3, duration:alto*1.3})
+    .setPin("#BD")
+    .addIndicators()
+    .addTo(controller);
 
 });
+function setDebug(){
+   $(window).scroll(function(evento){
+    $("#debug").html($(window).scrollTop());
+   })
+};
